4ecffec9b87f711b9119cc712280eef51c287fd1
Cuong Dong Minh <mcd.oblivion@gmail.com>
2022-11-22 17:59:54 +0700

    Add menu

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -1,17 +1,23 @@
 import { TFunction } from "i18next";
 import { ReactNode } from "react";
-import { ADMIN, NORMAL_USER } from "./consts";
+import { ADMIN, NORMAL_USER, RequestStatus } from "./consts";
 import {
   ADDRESS_ROUTE,
   CARPOOLING_GROUP_NORMAL_ROUTE,
   CARPOOLING_GROUP_ROUTE,
+  CARPOOLING_LOG_NORMAL_ROUTE,
   CARPOOLING_LOG_ROUTE,
+  CRON_JOB_ROUTE,
   DAY_OFF_REQUEST_NORMAL_ROUTE,
   DAY_OFF_REQUEST_ROUTE,
   DRIVERS_ROUTE,
+  LEAVE_GROUP_REQUEST_NORMAL_ROUTE,
   LEAVE_GROUP_REQUEST_ROUTE,
+  REGISTER_DRIVER_ROUTE,
   USER_ROUTE,
+  VEHICLES_NORMAL_ROUTE,
   VEHICLES_ROUTE,
+  WALLET_ROUTE,
 } from "./route-consts";
 
 export interface Menu {
@@ -24,19 +30,16 @@ export interface Menu {
 }
 const user = JSON.parse(localStorage.getItem("currentUserInfo"));
 export const menu: Menu[] = [
-  // {
-  //   name: "Đăng ký trở thành tài xế",
-  //   icon: "bx-home",
-  //   link: REGISTER_DRIVER_ROUTE,
-  //   show:
-  //     (!user?.driver ||
-  //       user?.driver?.status === "Pending" ||
-  //       user?.driver?.status === "Rejected") &&
-  //     user?.role === NORMAL_USER
-  //       ? true
-  //       : false,
-  //   active: false,
-  // },
+  {
+    name: "Đăng ký trở thành tài xế",
+    icon: "bx-home",
+    link: REGISTER_DRIVER_ROUTE,
+    show:
+      !user?.driver || user?.driver?.status !== RequestStatus.ACCEPTED
+        ? true
+        : false,
+    active: false,
+  },
   {
     name: "Địa chỉ",
     icon: "bx-location-plus",
@@ -44,21 +47,6 @@ export const menu: Menu[] = [
     show: user?.role === NORMAL_USER ? true : false,
     active: false,
   },
-  {
-    name: "Nhóm đi chung",
-    icon: "bx-group",
-    link: CARPOOLING_GROUP_NORMAL_ROUTE,
-    show: user?.role === NORMAL_USER ? true : false,
-    active: false,
-  },
-  {
-    name: "Yêu cầu nghỉ phép",
-    icon: "bx-question-mark",
-    link: DAY_OFF_REQUEST_NORMAL_ROUTE,
-    show: user?.role === NORMAL_USER && user?.carpoolingGroupId ? true : false,
-    active: false,
-  },
-
   {
     name: "Người dùng",
     icon: "bx-user",
@@ -76,36 +64,60 @@ export const menu: Menu[] = [
   {
     name: "Phương tiện",
     icon: "bx-car",
-    link: VEHICLES_ROUTE,
-    show: user?.role === ADMIN ? true : false,
-    active: false,
-  },
-  {
-    name: "Yêu cầu nghỉ phép",
-    icon: "bx-question-mark",
-    link: DAY_OFF_REQUEST_ROUTE,
-    show: user?.role === ADMIN ? true : false,
-    active: false,
-  },
-  {
-    name: "Lịch sử đi chung",
-    icon: "bx-home",
-    link: CARPOOLING_LOG_ROUTE,
-    show: user?.role === ADMIN ? true : false,
-    active: false,
-  },
-  {
-    name: "Yêu cầu rời nhóm",
-    icon: "bx-log-out-circle",
-    link: LEAVE_GROUP_REQUEST_ROUTE,
-    show: user?.role === ADMIN ? true : false,
+    link: user?.role === ADMIN ? VEHICLES_ROUTE : VEHICLES_NORMAL_ROUTE,
+    show: user?.role === ADMIN || !!user?.driver,
     active: false,
   },
   {
     name: "Nhóm đi chung",
     icon: "bx-group",
-    link: CARPOOLING_GROUP_ROUTE,
+    link:
+      user?.role === ADMIN
+        ? CARPOOLING_GROUP_ROUTE
+        : CARPOOLING_GROUP_NORMAL_ROUTE,
+    show: true,
+    active: false,
+  },
+  {
+    name: "Yêu cầu nghỉ phép",
+    icon: "bx-question-mark",
+    link:
+      user?.role === NORMAL_USER && user?.carpoolingGroupId
+        ? DAY_OFF_REQUEST_NORMAL_ROUTE
+        : DAY_OFF_REQUEST_ROUTE,
+    show: true,
+    active: false,
+  },
+  {
+    name: "Yêu cầu rời nhóm",
+    icon: "bx-log-out-circle",
+    link:
+      user?.role === ADMIN
+        ? LEAVE_GROUP_REQUEST_ROUTE
+        : LEAVE_GROUP_REQUEST_NORMAL_ROUTE,
+    show: true,
+    active: false,
+  },
+  {
+    name: "Lịch sử đi chung",
+    icon: "bx-home",
+    link:
+      user?.role === ADMIN ? CARPOOLING_LOG_ROUTE : CARPOOLING_LOG_NORMAL_ROUTE,
+    show: true,
+    active: false,
+  },
+  {
+    name: "Quản lý tác vụ",
+    icon: "bx-task",
+    link: CRON_JOB_ROUTE,
     show: user?.role === ADMIN ? true : false,
+    active: false,
+  },
+  {
+    name: "Ví C-Carpooling",
+    icon: "bx-wallet",
+    link: WALLET_ROUTE,
+    show: user?.role === NORMAL_USER,
     active: false,
   },
 ];
